0d803edc452f616ca56b683505455301bd9b8d53
Pacfist <knyazevmatvey4@gmail.com>
2025-07-04 18:02:15 +0500

    finished helper config

--- foundry-smart-lottery/script/HelperConfig.sol
+++ foundry-smart-lottery/script/HelperConfig.sol
@@ -3,8 +3,13 @@ pragma solidity ^0.8.8;
 
 import {Script} from "forge-std/Script.sol";
 import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
-
-contract HelperConfig is Script {
+abstract contract Constants {
+    uint96 public constant MOCK_BASE_FEE = 0.25 ether;
+    uint96 public constant MOCK_GAS_PRICE_LINK = 1e9;
+    // LINK / ETH price
+    int256 public constant MOCK_WEI_PER_UINT_LINK = 4e15;
+}
+contract HelperConfig is Script, Constants {
     error HelperConfig__InvalidChainId();
 
     struct NetworkConfig {
@@ -29,7 +34,7 @@ contract HelperConfig is Script {
     function getConfigByChainId(
         uint256 chainId
     ) public returns (NetworkConfig memory) {
-        if (networkConfig[chainId].vrfCoordinatorV2_5 != address(0)) {
+        if (networkConfig[chainId].vrfCoordinator != address(0)) {
             return networkConfig[chainId];
         } else if (chainId == 31337) {
             return getOrCreateAnvilEthConfig();
@@ -45,7 +50,7 @@ contract HelperConfig is Script {
                 interval: 30,
                 vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
                 gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
-                subscriptionId: 7679411351600780301149911877661372214661410357691824210932701096407150720747,
+                subscriptionId: 0,
                 callbackGasLimit: 500000
             });
     }
@@ -57,18 +62,33 @@ contract HelperConfig is Script {
                 interval: 30,
                 vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
                 gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
-                subscriptionId: 7679411351600780301149911877661372214661410357691824210932701096407150720747,
+                subscriptionId: 0,
                 callbackGasLimit: 500000
             });
     }
 
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
-        if (networkConfig.vrfCoordinator != address(0)) {
-            return networkConfig;
+        if (localNetworkConfig.vrfCoordinator != address(0)) {
+            return localNetworkConfig;
         }
 
         vm.startBroadcast();
-        VRFCoordinatorV2_5Mock vrfMock = new VRFCoordinatorV2_5Mock();
+        VRFCoordinatorV2_5Mock vrfMock = new VRFCoordinatorV2_5Mock(
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE_LINK,
+            MOCK_WEI_PER_UINT_LINK
+        );
         vm.stopBroadcast();
+
+        localNetworkConfig = NetworkConfig({
+            entranceFee: 1000000000000000,
+            interval: 30,
+            vrfCoordinator: address(vrfMock),
+            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+            subscriptionId: 0,
+            callbackGasLimit: 500000
+        });
+
+        return localNetworkConfig;
     }
 }
