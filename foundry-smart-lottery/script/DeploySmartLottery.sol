54e573d5649ff24afb9f369f0db71fe726e93746
Pacfist <knyazevmatvey4@gmail.com>
2025-07-07 23:06:37 +0500

    Interactions for creating sub

--- foundry-smart-lottery/script/DeploySmartLottery.sol
+++ foundry-smart-lottery/script/DeploySmartLottery.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.8;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.sol";
+import {CreateSubscription} from "./Interactions.sol";
 
-contract DeploySL is Script {
+contract DeploySL is Script, CreateSubscription {
     function run() public {
         deployContract();
     }
@@ -12,6 +13,13 @@ contract DeploySL is Script {
     function deployContract() public returns (Raffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        uint256 subID;
+
+        if (config.subscriptionId == 0) {
+            (subID, ) = CreateSubscription.createSub();
+        } else {
+            subID = config.subscriptionId;
+        }
 
         vm.startBroadcast();
         Raffle raffle = new Raffle(
@@ -19,7 +27,7 @@ contract DeploySL is Script {
             config.interval,
             config.vrfCoordinator,
             config.gasLane,
-            config.subscriptionId,
+            subID,
             config.callbackGasLimit
         );
         vm.stopBroadcast();
---
+++ foundry-smart-lottery/script/Interactions.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
+contract CreateSubscription is Script {
+    function createSub() public returns (uint256, address) {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+
+        console.log("Creating subscription on chain id:", block.chainid);
+        vm.startBroadcast();
+        uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordinator)
+            .createSubscription();
+        vm.stopBroadcast();
+        console.log("Subscription id: ", subId);
+        return (subId, vrfCoordinator);
+    }
+}
